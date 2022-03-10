815bd4f89c69db4441bf6da4d400e17b184fb1a2
MD AL AMIN <01mdalamin1@gmail.com>
2022-03-10 15:53:24 +0600

    first commit

---
+++ info.vueLaravel.php
@@ -0,0 +1,25 @@
+<?php 
+/* #vue ui 1-8> frontend || 9-34> backend
+vue create library_frontend => 
+cd library_frontend => 
+npm install vuex@next --save => 
+npm install vue-router@4 => 
+npm install => 
+npm run serve => 
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; ?>
