b8fa8adca8957997fedc49e21f2195f9ef8b1ed2
Sanjida Tonny <97305799+tanvironamica@users.noreply.github.com>
2023-07-16 00:59:53 +0600

    AdminLTE setup done

---
+++ virza_ms.php
@@ -0,0 +1,44 @@
+<?php 
+/* #vue ui 1-8> frontend || 9-34> backend
+vue create library_frontend => 
+cd library_frontend => 
+npm install vuex@next --save => 
+npm install vue-router@4 => 
+npm install => 
+npm run serve => 
+
+composer create-project --prefer-dist laravel/laravel:^10 laravel10
+
+composer create-project --prefer-dist laravel/laravel virza_ms
+cd virza_ms
+composer require laravel/passport
+composer require laravel/passport  --with-all-dependencies
+php artisan vendor:publish --tag=passport-migrations
+php artisan migrate
+php artisan passport:install
+
+composer require jeroennoten/laravel-adminlte |=> https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Installation
+
+
+
+php artisan serve
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; ?>
