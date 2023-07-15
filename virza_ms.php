df3386341362d0657a920fa4f69a28f3ca07b1be
Sanjida Tonny <97305799+tanvironamica@users.noreply.github.com>
2023-07-16 00:10:06 +0600

    start virza_ms
    it is school management system

---
+++ virza_ms.php
@@ -0,0 +1,40 @@
+<?php 
+/* #vue ui 1-8> frontend || 9-34> backend
+vue create library_frontend => 
+cd library_frontend => 
+npm install vuex@next --save => 
+npm install vue-router@4 => 
+npm install => 
+npm run serve => 
+
+composer create-project --prefer-dist laravel/laravel:^10 laravel10
+
+composer create-project --prefer-dist laravel/laravel virza_ms
+cd virza_ms
+composer require laravel/passport
+php artisan vendor:publish --tag=passport-migrations
+php artisan migrate
+php artisan passport:install
+
+
+php artisan serve
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; ?>
