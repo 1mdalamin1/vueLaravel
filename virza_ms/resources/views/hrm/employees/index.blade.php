ab33bb42790502dd66575bf1b271162584c887a0
Sanjida Tonny <97305799+tanvironamica@users.noreply.github.com>
2023-09-27 10:25:14 +0600

    from submit by ajax

--- virza_ms/resources/views/hrm/employees/index.blade.php
+++ virza_ms/resources/views/hrm/employees/index.blade.php
@@ -18,160 +18,13 @@
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">+ Add Employee</button>
 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <form action="{{route('hrm.store.employee')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Add New Employee</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        <div class="modal-body">
-            
-            <div class="row">
-                <div class="col-6">
-                            
-                    <div class="form-group">
-                        <label for="employee_id" class="form-label">Employee Id<span class="text-danger">*</span></label>
-                        <input type="text" name="employee_id" class="form-control" placeholder="Ex: e-123" value="{{old('employee_id')}}">
-                        @if($errors->has('employee_id'))
-                            <span class="text-danger">{{ $errors->first('employee_id') }}</span>
-                        @endif
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="user_id" class="form-label">Name</label>
-                        <select class="form-control multiple-not " id="user_id" data-placeholder="Select a department" name="user_id">
-                        @foreach ($users as $role)
-                            <option value="{{$role->id}}">{{$role->name}}</option>
-                        @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="department_id" class="form-label">Depertments</label>
-                        <select class="form-control multiple-not " id="department_id" data-placeholder="Select a department" name="department_id">
-                        @foreach ($depertments as $role)
-                            <option value="{{$role->id}}">{{$role->designation_name}}</option>
-                        @endforeach
-                        </select>
-                    </div>
+<!-- Modal on -->
 
-                </div>
-                <!-- End left side  -->
-                <div class="col-6">
 
-                    <div class="form-group">
-                        <label for="image" class="form-label">image<span class="text-danger">*</span></label>
-                        <input type="file" id="image" name="image" class="form-control" placeholder="Ex: e-123" value="{{old('image')}}">
-                        
+<!-- Modal off -->
 
-                        @if($errors->has('image'))
-                            <span class="text-danger">{{ $errors->first('image') }}</span>
-                        @endif
-                    </div>
-                    <img id="employee_image_preview" class="h-64 w-128 object-cover rounded-md" src="{{ old('image') ? Storage::url(old('image')) : '' }}" alt="Employee image preview" />
-                    <!-- <img id="featured_image_preview" class="h-64 w-128 object-cover rounded-md" src="{{ isset($post) ? Storage::url($post->featured_image) : '' }}" alt="Featured image preview" /> -->
 
-                    <!-- {{ session('imagePath') }} -->
 
-                </div>
-                <!-- End Right side side  -->
-            </div>
-
-            <div class="row">
-                <div class="col-6">
-
-                    <div class="form-group">
-                        <label for="blood" class="form-label">Blood Group<span class="text-danger">*</span></label>
-                        <input type="text" id="blood" name="blood" class="form-control" placeholder="Ex: e-123" value="{{old('blood')}}">
-                        @if($errors->has('blood'))
-                            <span class="text-danger">{{ $errors->first('blood') }}</span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="joining_date" class="form-label">Joining date<span class="text-danger">*</span></label>
-                        <input type="date" id="joining_date" name="joining_date" class="form-control" placeholder="Ex: e-123" value="{{old('joining_date')}}">
-                        @if($errors->has('joining_date'))
-                            <span class="text-danger">{{ $errors->first('joining_date') }}</span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="dob" class="form-label">Date of barth<span class="text-danger">*</span></label>
-                        <input type="date" id="dob" name="dob" class="form-control" placeholder="Ex: e-123" value="{{old('dob')}}">
-                        @if($errors->has('dob'))
-                            <span class="text-danger">{{ $errors->first('dob') }}</span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="gender" class="form-label">Gender<span class="text-danger">*</span></label>
-                        <select class="form-control multiple-not " id="gender" data-placeholder="Select a gender" name="gender">
-                            <option value="Male">Male</option>
-                            <option value="Female">Female</option>
-                            <option value="Other">Other</option>
-                        </select>
-                    </div>
-
-                </div>
-                <!--  End left side  --> 
-                <div class="col-6">
-                    
-                    <div class="form-group">
-                        <label for="phone" class="form-label">Phone<span class="text-danger">*</span></label>
-                        <input type="text" name="phone" id="phone" class="form-control" placeholder="01795xxxxxx" value="{{old('phone')}}">
-                        @if($errors->has('phone'))
-                            <span class="text-danger">{{ $errors->first('phone') }}</span>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="nid" class="form-label">Nid<span class="text-danger">*</span></label>
-                        <input type="text" id="nid" name="nid" class="form-control" placeholder="Ex: e-123" value="{{old('nid')}}">
-                        @if($errors->has('nid'))
-                            <span class="text-danger">{{ $errors->first('nid') }}</span>
-                        @endif
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="salary" class="form-label">Salary<span class="text-danger">*</span></label>
-                        <input type="text" id="salary" name="salary" class="form-control" placeholder="Ex: e-123" value="{{old('salary')}}">
-                        @if($errors->has('salary'))
-                            <span class="text-danger">{{ $errors->first('salary') }}</span>
-                        @endif
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="status" class="form-label">Status<span class="text-danger">*</span></label>
-                        <select class="form-control multiple-not " id="status" data-placeholder="Select a status" name="status">
-                            <option value="Active">Active</option>
-                            <option value="inActive">inActive</option>
-                        </select>
-                    </div>
-
-                </div>
-                <!-- End Right side side  -->
-            </div>
-                
-            <div class="form-group">
-                <label for="address" class="form-label">Address<span class="text-danger">*</span></label>
-                <input type="text" name="address" class="form-control" placeholder="Ex: e-123" value="{{old('address')}}">
-                @if($errors->has('address'))
-                    <span class="text-danger">{{ $errors->first('address') }}</span>
-                @endif
-            </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Save</button>
-        </div>
-      </div>
-
-    </form>
-      
-    </div>
-  </div>
-  
 
 
         <div class="row">
@@ -390,7 +243,46 @@
         });
 
         $(document).ready(function(){
+            
+            // Add an event listener for form submission
+            $('form').on('submit', function(event){
+                event.preventDefault(); // Prevent default form submission
                 
+                // Serialize the form data
+                // var formData = $(this).serialize();
+                var formData = new FormData(this); // Create a FormData object to handle file uploads
+                
+                // Send an AJAX request
+                $.ajax({
+                    url: $(this).attr('action'), // Use the form's action attribute
+                    type: $(this).attr('method'), // Use the form's method attribute
+                    data: formData,
+                    contentType: false, // Don't set content type (allows FormData to set it)
+                    processData: false, // Don't process data (allows FormData to process it)
+                    success: function(response) {
+                        // Handle the success response if needed
+                        console.log(response);
+                        $("#employeeData").DataTable().ajax.reload();
+                    },
+                    error: function(xhr, status, error) {
+                        // Handle the error response if needed
+                        let errorMessage = xhr.responseText;
+                        let errorJson = JSON.parse(xhr.responseText);
+                        console.log(errorJson.errors);
+                        $("#errorBox").html(`<div class="alert alert-danger">
+                        <h3>${errorJson.message}</h3>`+
+                            errorJson.errors.forEach(function(error) {
+                                `<p>${error[0]}</p>`;
+                            })+
+                            
+                        `</div>`);
+
+                    }
+                });
+            });
+
+
+            // Show Employee with Yajra DataTable
             let table = $('#employeeData').DataTable({
                 responsive:true, processing:true, serverSide:true, autoWidth:false,
                 ajax:"{{route('hrm.employee')}}",
@@ -406,7 +298,7 @@
                 order:[[0,"desc"]]
             });
 
-            
+            // Delete Employee javaScript
             $('body').on('click', '#btnDel', function(){
                 //confirmation
                 let id = $(this).data("id");
@@ -422,7 +314,7 @@
                             $("#employeeData").DataTable().ajax.reload();
                         },
                         error:function(res){
-                            $("#errorBox").html('<div class="alert alert-danger">'+res.message+'</div>')
+                            $("#errorBox").html('<div class="alert alert-danger">'+res.message+'</div>');
                         },
                     });
                 } else {
