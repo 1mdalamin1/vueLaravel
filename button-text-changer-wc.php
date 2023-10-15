55e3e7059a73659435dc4816ed92c5464b825818
Sanjida Tonny <97305799+tanvironamica@users.noreply.github.com>
2023-10-15 20:52:02 +0600

    Variables escaped done.

--- button-text-changer-wc.php
+++ button-text-changer-wc.php
@@ -93,7 +93,7 @@ function btcwc_button_checkout_texts() {
     ?> 
     <a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" 
     class="checkout-button button alt wc-forward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>">
-        <?php echo $pCheckout; ?>
+        <?php echo esc_html($pCheckout); ?>
     </a>
 
     <?php
